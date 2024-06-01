26cf8b43f292c53ec0bcf9562b7a535bdb53c640
Hurundu <hurundu@outlook.com>
2024-06-01 14:23:57 +0800

    add __init__.py to project

---
+++ __init__.py
@@ -0,0 +1,28 @@
+
+from coordrdinate import (
+    # Coo
+    Coo2D,
+    Coo3D,
+
+    # Space
+    Space,
+    Space2D,
+    Space3D,
+
+    # Coordinate
+    Coordinate,
+    Coordinate2D,
+    Coordinate3D,
+
+    # 2D
+    Cartesian2D,
+    Polar,
+
+    # 3D
+    Cartesian3D,
+    Cylindrical,
+    Spherical,
+
+    # functions
+    map_angle
+)
